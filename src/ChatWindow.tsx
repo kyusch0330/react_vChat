c57a253b70305a76caa565c6ec387111a6a94d7b
kschoi <kyusch0330@gmail.com>
2021-06-26 01:58:06 +0900

    Apply React Hook to all components

--- src/ChatWindow.tsx
+++ src/ChatWindow.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 import ChatDisplay from './ChatDisplay'; 
 import ChatBar from './ChatBar'; 
 
@@ -16,30 +16,31 @@ type Props = {
   onRegister:CallableFunction;
 }
 
+const ChatWindow = (props:Props) => {
+  const displayRef = useRef<HTMLInputElement>(null);
 
-export default class ChatWindow extends React.Component<Props>{
-  private displayRef = React.createRef<HTMLInputElement>();
-
-  componentDidUpdate(){
-    if(this.displayRef.current){
-      this.displayRef.current.scrollTop = this.displayRef.current.scrollHeight;
+  useEffect(() => {
+    if(displayRef.current){
+      displayRef.current.scrollTop = displayRef.current.scrollHeight;
     }
+  });
+
+  const handleChatRegister = (chat:string) => {
+    props.onRegister(props.user, chat);
   }
 
-  handleChatRegister = (chat:string) => {
-    this.props.onRegister(this.props.user, chat);
-  }
 
-  render(){
-    const chatLogs:ChatLog[] = this.props.chatLogs;
-    return (
-      <React.Fragment>
-        <h1 style={{color:"white"}}>{this.props.user.name}</h1>
-        <div className="chatDisplay" ref={this.displayRef}>
-          <ChatDisplay id={this.props.user.id} chatLogs={chatLogs}/>
-        </div>
-        <ChatBar onRegister={this.handleChatRegister}/>
-      </React.Fragment>
-    )
-  }
+  const chatLogs:ChatLog[] = props.chatLogs;
+  return (
+    <React.Fragment>
+      <h1 style={{color:"white"}}>{props.user.name}</h1>
+      <div className="chatDisplay" ref={displayRef}>
+        <ChatDisplay id={props.user.id} chatLogs={chatLogs}/>
+      </div>
+      <ChatBar onRegister={handleChatRegister}/>
+    </React.Fragment>
+  )
 }
+
+export default ChatWindow;
+
