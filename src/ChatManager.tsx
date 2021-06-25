46aa9f9d3fe46fa929d7164df5608eb1e8c3f0c2
kschoi <kyusch0330@gmail.com>
2021-06-25 23:57:46 +0900

    Apply react Hook

--- src/ChatManager.tsx
+++ src/ChatManager.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ChatWindow  from './ChatWindow';
 import "./Chat.css";
 
@@ -16,44 +16,75 @@ type ChatLog = {
   chat:string;
 }
 
-type State = {
-  chatLogs: ChatLog[];
-}
+export default function ChatManager(props:Props) {
+  const [chatLogs, setChatLogs] = useState<ChatLog[]>([]);
+  
+  const handleChatRegister = (user:User,chat:string) => {
+    //객체 불변성을 위해 push 대신 concat을 사용한다.
+    const newLogs = chatLogs.concat([{user: user, chat: chat}]);
 
-export default class ChatManager extends React.Component<Props,State>{
-
-  constructor(props:Props) {
-    super(props);
-    this.state = {chatLogs: []};
+    // let newLogs = chatLogs;
+    // newLogs.push({
+    //   user: user, 
+    //   chat: chat,
+    // }); //-> 작동안됨
+    setChatLogs(newLogs);
   }
-  handleChatRegister = (user:User,chat:string) => {
-    let newLogs = this.state.chatLogs;
-    newLogs.push({
-      user: user, 
-      chat: chat,
-    });
-    this.setState({
-      chatLogs : newLogs,
-    });
-  }
-
-  render() {
-    const chatWindows = this.props.users.map((user:User) => {
-      return (
-        <div className="chatWindow" key={user.id}>
-          <ChatWindow  
-           user={user}
-           onRegister={this.handleChatRegister}
-           chatLogs={this.state.chatLogs}
-           />
-        </div>
-      );
-    }); 
-
+  
+  const chatWindows = props.users.map((user:User) => {
     return (
-      <div className="chatManager">
-        {chatWindows}
+      <div className="chatWindow" key={user.id}>
+        <ChatWindow  
+         user={user}
+         onRegister={handleChatRegister}
+         chatLogs={chatLogs}
+         />
       </div>
     );
-  }
+  }); 
+  
+  return (
+    <div className="chatManager">
+      {chatWindows}
+    </div>
+  );
+
 }
+
+// export default class ChatManager extends Component<Props,State>{
+
+//   constructor(props:Props) {
+//     super(props);
+//     this.state = {chatLogs: []};
+//   }
+//   handleChatRegister = (user:User,chat:string) => {
+//     let newLogs = this.state.chatLogs;
+//     newLogs.push({
+//       user: user, 
+//       chat: chat,
+//     });
+//     this.setState({
+//       chatLogs : newLogs,
+//     });
+//   }
+
+//   render() {
+//     const chatWindows = this.props.users.map((user:User) => {
+//       return (
+//         <div className="chatWindow" key={user.id}>
+//           <ChatWindow  
+//            user={user}
+//            onRegister={this.handleChatRegister}
+//            chatLogs={this.state.chatLogs}
+//            />
+//         </div>
+//       );
+//     }); 
+
+//     return (
+//       <div className="chatManager">
+//         {chatWindows}
+//       </div>
+//     );
+//   }
+// }
